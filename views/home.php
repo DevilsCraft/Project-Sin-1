9630c6c1e44cce2c78074c86f4e40574e396e8f3
Quentin Aslan <quentin.aslan@outlook.com>
2016-10-15 20:50:52 +0200

    Ajout de texte et de pages

--- views/home.php
+++ views/home.php
@@ -1,7 +1,17 @@
 <h1>Le stockage des données numérique</h1>
 
 <div class="text">
-	
-	ici du texte
+
+	Le choix de la méthode de stockage se fait selon plusieurs critères :
+	<ul>
+		<li>La fréquence d'utilisation(Combien de fois par heure/jours/semaines ...)</li>
+		<li>Les besoins capacitaires de l'information (taille des données)</li>
+		<li>la criticité de l'information (Importance de l'information)</li>
+	</ul>
+
+	<h4>Sur ce site vous trouverez les nombreuses méthode que nous utilisont au quotidiens pour stocker nos données numérique !</h4>
+
+	<br /><br /> <br />
+	<a href="https://github.com/DevilsCraft/Project-Sin-1">GitHub :p</a>
 
 </div>
